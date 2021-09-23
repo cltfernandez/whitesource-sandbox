879c4267653e0354ddadca9de4650cbbe3ded962
cltfernandez <chris.fernandez@ofx.com>
2021-09-23 13:42:32 +0800

    Initial Commit

---
+++ VulnerableApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VulnerableApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
